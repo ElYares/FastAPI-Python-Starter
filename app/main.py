15e3debc2ba88c1ca70bd702a358b6df054500c3
ElYares <yared.elizondo.ingsoft@gmail.com>
2025-07-23 21:11:59 -0600

    chore: se incluyen las rutas del api

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.api.v1.routes import router
 from app.api.v1.health_routes import router as health_router
+from app.api.v1.auth_routes import router as auth_router
+from app.api.v1.secure_routes import router as secure_router
 
 """
 Punto de entrada de la aplicacion FastAPI
@@ -19,3 +21,5 @@ app = FastAPI(
 # Todas las rutas estaran bajo el prefijo /api/v1
 app.include_router(router, prefix="/api/v1")
 app.include_router(health_router, prefix="/api/v1")
+app.include_router(auth_router, prefix="/api/v1")
+app.include_router(secure_router, prefix="/api/v1")
