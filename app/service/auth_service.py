f6aaa4c2d8e039a88a1c19e8c50dbd8b8f9a2808
ElYares <yared.elizondo.ingsoft@gmail.com>
2025-12-27 22:28:09 -0600

    fix(auth): Timezone import and jwt token creation consistency.

--- app/service/auth_service.py
+++ app/service/auth_service.py
@@ -1,20 +1,23 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from jose import jwt
 from app.config import settings
 
+
 class AuthService:
     """
-    Servicio encargado de generar y verificar JWTs.
+    Servicio encargado de generar JWTs.
     """
 
     def create_access_token(self, data: dict) -> str:
         """
         Genera un JWT usando los datos y tiempo de expiracion
         """
-
         to_encode = data.copy()
-        expire = datetime.utcnow() + timedelta(minutes=settings.JWT_EXPIRE_MINUTES)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=settings.JWT_EXPIRE_MINUTES)
         to_encode.update({"exp": expire})
-        encoded_jwt = jwt.encode(to_encode, "secret", algorithm=settings.JWT_ALGORITHM)
-        return  encoded_jwt
 
+        return jwt.encode(
+            to_encode,
+            settings.JWT_SECRET_KEY,
+            algorithm=settings.JWT_ALGORITHM,
+        )
